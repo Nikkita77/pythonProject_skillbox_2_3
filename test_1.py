6236684c30a01c96dcd395e581d01eec5e902ad5
nikita <nikitavik89@gmail.com>
2023-03-19 11:32:01 +0300

    добавлена конфигурация toxi файл

--- test_1.py
+++ test_1.py
@@ -1,35 +1,35 @@
 import time
 import pytest
+import allure
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 class TestExample:  # Teстовый набор
+    @allure.description("Test open url")
     def test_1(self, set_up):
-        options = ChromeOptions()
-        options.headless = True
-        driver = Chrome(options=options)
-        driver.get('https://go.skillbox.ru/')
-        assert 'Обучающая онлайн-платформа Skillbox' == driver.title
-        time.sleep(3)
-        driver.quit()
+        with allure.step("open url"):
+            options = ChromeOptions()
+            # options.headless = True
+            driver = Chrome(options=options)
+            driver.get('https://go.skillbox.ru/')
+            assert 'Skillbox' == driver.title
+            driver.quit()
 
     def test_2(self, set_up):
         options = ChromeOptions()
-        options.headless = True
+        # options.headless = True
         driver = Chrome(options=options)
         driver.get('https://go.skillbox.ru/')
-        assert 'Обучающая онлайн-платформа Skillbox' == driver.title
-        time.sleep(3)
+        assert 'Skillbox' == driver.title
         driver.quit()
 
     def test_3(self, set_up):
         options = ChromeOptions()
-        options.headless = True
+        # options.headless = True
         driver = Chrome(options=options)
         driver.get('https://go.skillbox.ru/')
-        assert 'Обучающая онлайн-платформа Skillbox' == driver.title
-        time.sleep(3)
+        assert 'Skillbox' == driver.title
         driver.quit()
 
 
