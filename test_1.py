d1f36c9c0da98e7f67f94e5df37ccfdea17bc243
Nikita Vikulin <nikitavik89@gmail.com>
2023-03-19 13:10:07 +0300

    Добавлен шаг Allure

--- test_1.py
+++ test_1.py
@@ -5,15 +5,20 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
+@allure.step('Проверка заголовка страницы')
+def check_title(driver, title):
+    assert title == driver.title
+
+
 class TestExample:  # Teстовый набор
     @allure.description("Test open url")
-    def test_1(self, set_up):
+    def test_1(self, set_up):  # Тест-кейс
         with allure.step("open url"):
             options = ChromeOptions()
             # options.headless = True
             driver = Chrome(options=options)
             driver.get('https://go.skillbox.ru/')
-            assert 'Skillbox' == driver.title
+            check_title(driver=driver, title='Обучающая онлайн-платформа Skillbox')
             driver.quit()
 
     def test_2(self, set_up):
@@ -31,7 +36,3 @@ class TestExample:  # Teстовый набор
         driver.get('https://go.skillbox.ru/')
         assert 'Skillbox' == driver.title
         driver.quit()
-
-
-
-
